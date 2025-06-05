b091030216c9815373bd4d82fef05c1e7b843f5e
MinWu CHO <mauercho@gmail.com>
2025-06-05 19:56:33 +0900

    feat: friend router 생성

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,20 @@
-require('dotenv').config();
-const express       = require('express');
-const cookieParser  = require('cookie-parser');
-const userRoutes    = require('./src/routes/user.routes');
-const scheduleRoutes    = require('./src/routes/schedule.routes');
-const contestRouter = require('./src/routes/contest.routes');
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./src/routes/user.routes");
+const scheduleRoutes = require("./src/routes/schedule.routes");
+const contestRouter = require("./src/routes/contest.routes");
+const friendRouter = require("./src/routes/friend.routes");
 
-const app  = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(express.json());
-app.use('/', userRoutes);
-app.use('/', scheduleRoutes);
-app.use('/', contestRouter);
-
+app.use("/", userRoutes);
+app.use("/", scheduleRoutes);
+app.use("/", contestRouter);
+app.use("/", friendRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
